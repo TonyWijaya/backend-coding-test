8d9e08fced033487137c548b3d6334e4106a2cf5
Tony <tony.wij@live.com>
2019-12-12 20:19:18 +0700

    Add eslint tooling
    - Install package 'eslint'
    - Create eslint configuration based on following:
    <> To check syntax, find problems, and enforce code style
    <> Type of modules: CommonJS (require / exports)
    <> Framework: None
    <> Typescript support: Yes
    <> Style of project: Standard(https://github.com/standard/standard)
    <> Config file format: .JSON
    - Install package 'typescript'
    - Add 'mocha' in env at eslint configuration
    - Add pretest script in package.json to do eslint
    - Automate eslint fixing with 'npm run pretest -- --fix'
    - Remove several unused variables at index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const app = express();
-const port = 8010;
+const port = 8010
 
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database(':memory:')
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-
-const buildSchemas = require('./src/schemas');
+const buildSchemas = require('./src/schemas')
 
 db.serialize(() => {
-    buildSchemas(db);
+  buildSchemas(db)
 
-    const app = require('./src/app')(db);
+  const app = require('./src/app')(db)
 
-    const swaggerUi = require('swagger-ui-express');
-    const openApiDocumentation = require('./docs/open_api_docs.json');
+  const swaggerUi = require('swagger-ui-express')
+  const openApiDocumentation = require('./docs/open_api_docs.json')
 
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation))
 
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
-});
+  app.listen(port, () => console.log(`App started and listening on port ${port}`))
+})
