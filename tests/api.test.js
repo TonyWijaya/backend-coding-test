5f3dcd0fdb17831246cc859a3bd1bce528d0711d
Tony <tony.wij@live.com>
2019-12-12 22:12:13 +0700

    Add testing 'GET /rides'
    - Add testing to get all rides endpoint
    - Scenario: Rides are empty

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,6 +8,7 @@ const db = new sqlite3.Database(':memory:')
 const app = require('../src/app')(db)
 const buildSchemas = require('../src/schemas')
 
+/** ** API Testing ***/
 describe('API tests', () => {
   before((done) => {
     db.serialize((err) => {
@@ -21,12 +22,29 @@ describe('API tests', () => {
     })
   })
 
+  // Testing get healthiness of API endpoint
   describe('GET /health', () => {
     it('should return health', (done) => {
       request(app)
         .get('/health')
         .expect('Content-Type', /text/)
         .expect(200, done)
+    })
+  })
+
+  // Testing get all rides endpoint
+  // Rides are empty
+  describe('GET /rides', () => {
+    it('should respond with 404 Bad Request: no rides found', (done) => {
+      request(app)
+        .get('/rides')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .end((err) => {
+          if (err) return done(err)
+          done()
+        })
     })
   })
 })
